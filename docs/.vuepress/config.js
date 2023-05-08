a2f0754056273524d40bf302f77a4e6520611f84
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2023-05-08 11:52:16 +0900

    first commit for test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    base: '/',
+    lang: 'ja-JP',
+    title: 'いかすきーサポート',
+    description: 'Misskeyサーバー「いかすきー」のサポートサイトです',
+})
