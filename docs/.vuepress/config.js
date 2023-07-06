a6cf83d6d4b35579c07fcd099d24ff9cbc4e4385
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2023-07-06 16:10:07 +0900

    add: new terms.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,11 +22,29 @@ export default defineUserConfig({
             { text: 'メンテナンス', link: '/category/メンテナンス/' },
             {
                 text: '利用規約',
-                children: [{ text: '利用規約全文', link: '/terms/' }, { text: '変更履歴', link: '/terms/changelog.html' }]
+                children: [
+                    {
+                        text: '現行規約',
+                        children: [
+                            { text: '利用規約全文', link: '/terms/' },
+                            { text: '変更履歴', link: '/terms/changelog.html' },
+                            { text: 'モデレーション関連事項', link: '/terms/other-terms.html' },
+                        ]
+                    },
+                    {
+                        text: '旧規約',
+                        children: [
+                            { text: '利用規約全文', link: '/terms/terms_v1.html' },
+                        ]
+                    }
+                ]
             },
             {
                 text: 'プライバシーポリシー',
-                children: [{ text: 'プライバシーポリシー全文', link: '/privacy-policy/' }, { text: '変更履歴', link: '/privacy-policy/changelog.html' }]
+                children: [
+                    { text: 'プライバシーポリシー全文', link: '/privacy-policy/' },
+                    { text: '変更履歴', link: '/privacy-policy/changelog.html' }
+                ]
             },
             { text: 'いかすきー', link: 'https://ikaskey.bktsk.com/', target: '_blank' },
         ]
